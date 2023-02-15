56150a3918d8893764fa749b5c9c157b74819215
skim0 <mail-github@seongha.kim>
2023-02-15 18:10:42 +0000

    Catching up

---
+++ fullstack-rubyonrails/ruby/tictactoe/tictactoe.py
@@ -0,0 +1,6 @@
+# Tic Tac Toe from The Odin Project
+# https://www.theodinproject.com/lessons/ruby-tic-tac-toe
+
+# Implement a game of tic-tac-toe, use OOP concepts
+# Using Python for now
+
--- fullstack-rubyonrails/ruby/tictactoe/tictactoe.rb
+++ fullstack-rubyonrails/ruby/tictactoe/tictactoe.rb
@@ -1,8 +1,29 @@
-# Tic Tac  Toe from The Odin Project
+# Tic Tac Toe from The Odin Project
 # https://www.theodinproject.com/lessons/ruby-tic-tac-toe
 
 # Implement a game of tic-tac-toe, use OOP concepts
 
-def main
+# the following aren't oop, should re-write
+
+def check_winner(board)
     return 0
 end
+
+def print_board(board)
+    puts "+-BOARD-+"
+    puts "| #{board[0]} #{board[1]} #{board[2]} |"
+    puts "| #{board[3]} #{board[4]} #{board[5]} |"
+    puts "| #{board[6]} #{board[7]} #{board[8]} |"
+    puts "+-------+"
+end
+
+def advance_round(board)
+    return 0
+end
+
+def main
+    board = [0,0,0,0,0,0,0,0,0]
+
+    return 0
+end
+
